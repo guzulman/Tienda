77e20f5721343d5831dea0f5e5943d3ee620be83
guzulman <123052175+guzulman@users.noreply.github.com>
2023-03-18 02:25:09 -0600

    Commit semana 9

---
+++ src/main/java/com/tienda/WebConfig.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tienda;
+
+import java.util.Locale;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+/**
+ *
+ * @author jcga5
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+    
+    @Bean
+    public SessionLocaleResolver localResolver(){
+        var slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(new Locale("es"));
+        return slr;
+    }
+    
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        var lci = new LocaleChangeInterceptor();
+        lci.setParamName("");
+        return lci;
+    }
+    
+    @Override
+    public void addInterceptors(InterceptorRegistry registro){
+        registro.addInterceptor(localeChangeInterceptor());
+    }
+    
+}
